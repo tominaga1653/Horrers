ec4bef3b2fac515ba202889f04474ef1034231dc
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-09-12 03:20:35 +0000

    [add]コメント機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,11 @@
 class Admin::UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.page(params[:page]).per(20)
   end
 
   def edit
--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,9 +1,22 @@
 class Public::CommentsController < ApplicationController
 
   def create
+    post = Post.find(params[:post_id])
+    comment = current_user.comments.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(post)
   end
 
   def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to post_path(params[:post_id]), notice: "コメントを削除しました。"
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 
 end
