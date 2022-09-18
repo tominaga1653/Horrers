f1da0def6678388f458fb441dd934b5bcc2c4fea
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-09-18 10:27:17 +0000

    [add]seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(
+  name: '富永',
+  email: 'admin@sample.com',
+  password: 'admin1234'
+  )
