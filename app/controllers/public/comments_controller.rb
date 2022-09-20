7a9ae1f6d845c24e7869dccae2e2af226de0b8f0
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-09-20 10:58:04 +0000

    [update]コメントの非同期通信化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,16 +3,18 @@ class Public::CommentsController < ApplicationController
   before_action :ensure_correct_user, only: [:destroy]
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post), notice: "コメントを投稿しました。"
+    @comment = Comment.new
+    flash.now[:notice] = "コメントを投稿しました。"
   end
 
   def destroy
     Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id]), notice: "コメントを削除しました。"
+    @post = Post.find(params[:post_id])
+    flash.now[:notice] = "コメントを削除しました。"
   end
 
   private
